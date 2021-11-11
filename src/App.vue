9d923e6c6082cb6862103489d4b43c9cb787c1a5
nihao <hao15710065945@163.com>
2021-11-11 13:54:38 +0800

    master提交

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <div class="rou">
+    <router-link to="/">home</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style >
+* {
+  margin: 0px;
+  padding: 0px;
+  user-select: none;
+  text-decoration: none;
+  list-style: none;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.rou a {
+  font-size: 30px;
+  color: rebeccapurple;
+}
+.rou a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
