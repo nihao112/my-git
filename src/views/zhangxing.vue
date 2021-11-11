c890b284c6cb809a555ba4bf2442edf337754401
nihao <hao15710065945@163.com>
2021-11-11 17:33:05 +0800

    dev分支提交

--- src/views/zhangxing.vue
+++ src/views/zhangxing.vue
@@ -1,10 +1,13 @@
-<template>
-    <h1>我是张星</h1>
-</template>
 
 <script setup>
-
+import Add from '../components/Add.vue'
+import { ref } from 'vue'
+var str = ref(1)
 </script>
+<template>
+  <h1>我是张星</h1>
+  <Add :str="str" />
+</template>
 
 <style>
 </style>
